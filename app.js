6c8b58d8730c84ef09785b0d6cc38e13065e86e8
chiru.thripura <chiru.thripura@koyadocutracker.com>
2016-04-28 12:49:48 +0530

    dsd

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express = require('express');
 var app = express();
 
 
-app.get('/',function(req,res){
+app.get('/sample',function(req,res){
     res.send("hai");
 });
 app.listen(3500,function(){
