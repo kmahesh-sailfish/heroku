2e1bc69bf957f77aff7fee1fcb6f4603b7b8d985
chiru.thripura <chiru.thripura@koyadocutracker.com>
2016-04-28 12:43:04 +0530

    dsd

---
+++ app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by sailfish on 4/28/2016.
+ */
+'use strict';
+
+var express = require('express');
+var app = express();
+
+
+app.get('/',function(req,res){
+    res.send("hai");
+});
+app.listen(3500,function(){
+    console.log("listen the port no is 3500");
+});
